66032c2e3710f6ba8bfb51532001546de54614f8
Arthur Vivian <arthur@rive.app>
2021-06-07 12:59:25 +0100

    Export rive-js types

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ import useStateMachineInput from './hooks/useStateMachineInput';
 export default Rive;
 export { useRive, useStateMachineInput };
 export { RiveState, UseRiveParameters, UseRiveOptions } from './types';
+export * from 'rive-js';

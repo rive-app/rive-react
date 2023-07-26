f8aa50fa498493428a81a169f36daa0e331b4da5
Slava Khanilo <52448226+khanilov@users.noreply.github.com>
2023-07-26 18:13:57 +0300

    feature: expose RiveProps type from package

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import Rive from './components/Rive';
+import Rive, { RiveProps } from './components/Rive';
 import useRive from './hooks/useRive';
 import useStateMachineInput from './hooks/useStateMachineInput';
 import useResizeCanvas from './hooks/useResizeCanvas';
 
 export default Rive;
-export { useRive, useStateMachineInput, useResizeCanvas };
+export { useRive, useStateMachineInput, useResizeCanvas, RiveProps };
 export { RiveState, UseRiveParameters, UseRiveOptions } from './types';
 export * from '@rive-app/canvas';

a7875b26a43342f6380a5009b25b831acfcfb610
Zach Plata <plata.zach@gmail.com>
2024-03-22 08:32:54 -0700

    feat: add new webgl2 package for new Rive Renderer

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,7 @@ set -e
 cp -r ./dist ./npm/react-webgl
 cp -r ./dist ./npm/react-canvas
 cp -r ./dist ./npm/react-canvas-lite
+cp -r ./dist ./npm/react-webgl2
 
 echo "Replacing the canvas with webgl references in react-webgl"
 pushd ./npm/react-webgl/dist
@@ -15,6 +16,17 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 else
   find . -type f -name "*.ts" -print0 | xargs -0 sed -i -e 's/@rive-app\/canvas/@rive-app\/webgl/g'
   find . -type f -name "*.js" -print0 | xargs -0 sed -i -e 's/@rive-app\/canvas/@rive-app\/webgl/g'
+fi
+popd
+
+echo "Replacing the canvas with webgl2 references in react-webgl2"
+pushd ./npm/react-webgl2/dist
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  find . -type f -name "*.ts" -print0 | xargs -0 sed -i '' -e 's/@rive-app\/canvas/@rive-app\/webgl2/g'
+  find . -type f -name "*.js" -print0 | xargs -0 sed -i '' -e 's/@rive-app\/canvas/@rive-app\/webgl2/g'
+else
+  find . -type f -name "*.ts" -print0 | xargs -0 sed -i -e 's/@rive-app\/canvas/@rive-app\/webgl2/g'
+  find . -type f -name "*.js" -print0 | xargs -0 sed -i -e 's/@rive-app\/canvas/@rive-app\/webgl2/g'
 fi
 popd
 

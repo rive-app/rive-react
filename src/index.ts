16dc257b2f904d51101535002b9fb34640a65842
Adam <67035612+damzobridge@users.noreply.github.com>
2024-05-23 17:11:50 -0700

    feat: create useRiveFile hook

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import Rive, { RiveProps } from './components/Rive';
 import useRive from './hooks/useRive';
 import useStateMachineInput from './hooks/useStateMachineInput';
 import useResizeCanvas from './hooks/useResizeCanvas';
+import useRiveFile from './hooks/useRiveFile';
 
 export default Rive;
-export { useRive, useStateMachineInput, useResizeCanvas, RiveProps };
-export { RiveState, UseRiveParameters, UseRiveOptions } from './types';
+export { useRive, useStateMachineInput, useResizeCanvas, useRiveFile , RiveProps };
+export { RiveState, UseRiveParameters, UseRiveFileParameters, UseRiveOptions } from './types';
 export * from '@rive-app/canvas';

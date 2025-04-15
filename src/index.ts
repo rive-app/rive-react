76eb68c8fad5a8e3dc859bf86114062860264524
Adam <67035612+damzobridge@users.noreply.github.com>
2025-04-15 15:18:38 -0700

    fix: remove viewmodel hooks from initial release

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
 import Rive, { RiveProps } from './components/Rive';
 import useRive from './hooks/useRive';
 import useStateMachineInput from './hooks/useStateMachineInput';
-import useViewModel from './hooks/useViewModel';
-import useViewModelInstance from './hooks/useViewModelInstance';
-import useViewModelInstanceNumber from './hooks/useViewModelInstanceNumber';
-import useViewModelInstanceString from './hooks/useViewModelInstanceString';
-import useViewModelInstanceBoolean from './hooks/useViewModelInstanceBoolean';
-import useViewModelInstanceColor from './hooks/useViewModelInstanceColor';
-import useViewModelInstanceEnum from './hooks/useViewModelInstanceEnum';
-import useViewModelInstanceTrigger from './hooks/useViewModelInstanceTrigger';
-import useViewModelProperties from './hooks/useViewModelProperties';
 import useResizeCanvas from './hooks/useResizeCanvas';
 import useRiveFile from './hooks/useRiveFile';
 
@@ -19,15 +10,6 @@ export {
   useStateMachineInput,
   useResizeCanvas,
   useRiveFile,
-  useViewModel,
-  useViewModelInstance,
-  useViewModelInstanceNumber,
-  useViewModelInstanceString,
-  useViewModelInstanceBoolean,
-  useViewModelInstanceColor,
-  useViewModelInstanceEnum,
-  useViewModelInstanceTrigger,
-  useViewModelProperties,
   RiveProps,
 };
 export {

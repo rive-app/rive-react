a0f7ad588300f50e44554c3b51279218934a0299
Lance <lance@lancesnider.com>
2025-04-25 12:57:34 -0600

    General cleanup

--- examples/.storybook/preview.ts
+++ examples/.storybook/preview.ts
@@ -1,11 +1,13 @@
-import type { Preview } from '@storybook/react'
+import type { Preview } from '@storybook/react';
+
+import './stories.css';
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
---
+++ examples/src/components/Events.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import { useRive, EventType, RiveEventType } from '@rive-app/react-canvas';
+
+const Events = () => {
+  const { rive, RiveComponent } = useRive({
+    src: 'rating.riv',
+    stateMachines: 'State Machine 1',
+    autoplay: true,
+    automaticallyHandleEvents: true,
+  });
+
+  const onRiveEventReceived = (riveEvent) => {
+    console.log('Rive event received:', riveEvent);
+    const eventData = riveEvent.data;
+    const eventProperties = eventData.properties;
+    if (eventData.type === RiveEventType.General) {
+      console.log('Event name', eventData.name);
+      console.log('Rating', eventProperties.rating);
+      console.log('Message', eventProperties.message);
+    }
+  };
+
+  // Wait until the rive object is instantiated before adding the Rive
+  // event listener
+  useEffect(() => {
+    if (rive) {
+      rive.on(EventType.RiveEvent, onRiveEventReceived);
+    }
+  }, [rive]);
+
+  return <RiveComponent />;
+};
+
+export default Events;
